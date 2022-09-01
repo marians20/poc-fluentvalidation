6c6013f10d89fa2e82209a7ae9696ce4c122ac5c
Marian Spoiala <mspoiala@microsoft.com>
2022-09-01 17:16:28 +0300

    Initial commit

---
+++ FluentValidationsPoc/FluentValidationsPoc/PersonDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentValidationsPoc;
+
+internal class PersonDto
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public int Rating { get; set; }
+}
---
+++ FluentValidationsPoc/FluentValidationsPoc/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FluentValidationsPoc;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var dto = new PersonDto
+{
+    Id = Guid.NewGuid(),
+    Name = null,
+    BirthDate = DateTime.Today.AddYears(-22)
+};
+
+var validator = new PersonDtoValidator();
+
+var result = validator.Validate(dto);
+
+Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+}));
